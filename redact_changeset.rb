6680e3911066b388766ec3d631aa129bdb840da8
Anton Khorev <tony29@yandex.ru>
2024-02-11 18:36:35 +0300

    Use only necessary OAuth2 client options with timeouts

--- redact_changeset.rb
+++ redact_changeset.rb
@@ -24,10 +24,8 @@ class Server
     unless @dry_run
       @client = OAuth2::Client.new y["oauth2"]["client_id"],
                                    nil,
-                                   {:site=>y["oauth2"]["site"],
-                                    :redirect_uri=>"urn:ietf:wg:oauth:2.0:oob",
-                                    :authorize_url=>"/oauth2/authorize",
-                                    :token_url=>"/oauth2/token"}
+                                   { :site => y["oauth2"]["site"],
+                                     :connection_opts =>{ :request => { :open_timeout => 320, :read_timeout => 320 } } }
 
       # Create the access_token for all traffic
       @access_token = OAuth2::AccessToken.new(@client, y["oauth2"]["token"])
