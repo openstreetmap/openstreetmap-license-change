315e6ef5295b89fa9fd1caf23adc1b51d602d86b
Anton Khorev <tony29@yandex.ru>
2024-02-12 17:06:54 +0300

    Don't raise errors on failed requests

--- auth.rb
+++ auth.rb
@@ -3,6 +3,7 @@ module Auth
     client = OAuth2::Client.new y["oauth2"]["client_id"],
                                 nil,
                                 { :site => y["oauth2"]["site"],
+                                  :raise_errors => false,
                                   :connection_opts =>{ :request => { :open_timeout => 320, :read_timeout => 320 } } }
 
     OAuth2::AccessToken.new(client, y["oauth2"]["token"])
